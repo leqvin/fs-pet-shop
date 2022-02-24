59ce2c2d63a3b48b7b05349cfa7b885e0bb9c01b
leqvin <levin@oregonstate.edu>
2022-02-24 13:41:21 -0800

    day 2, POST bonus complete

---
+++ httpServer.js
@@ -0,0 +1,116 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
+
+var http = require('http');
+var port = process.env.PORT || 8000;
+
+const petRegExp = /^\/pets\/(\d*)$/;
+
+var server = http.createServer(function(req, res) {
+  if (req.method === "GET" && petRegExp.test(req.url)){
+      const index = req.url.match(petRegExp)[1]
+      fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+        if (err) {
+          console.error(err.stack);
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'text/plain');
+          return res.end('Internal Server Error');
+        } else{
+            var pets = JSON.parse(petsJSON);
+            var petsJSONstr = JSON.stringify(pets[index]);
+            if(index < 0 || index > pets.length-1){
+                res.statusCode = 404;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Not Found');
+            } else {
+                res.setHeader('Content-Type', 'application/json');
+                res.write(petsJSONstr)
+                res.end();
+            }  
+        }
+      });
+  }  
+  else if (req.method === 'GET' && req.url === '/pets') {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Error Reading File');
+      } else{
+        res.setHeader('Content-Type', 'application/json');
+        res.write(petsJSON)
+        res.end();
+      }
+    });
+  }   
+  else if (req.method === 'POST' && req.url === '/pets') {
+    var body = "";
+    req.on('data', function (chunk) {
+        body += chunk;
+      });
+    // req.on('end', function () {
+    //     console.log('POSTed: ' + body);
+    //     res.writeHead(200);
+    //     res.end();
+    // });
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Error Reading File');
+      } else {
+        const neededKeys = ['age', 'kind', 'name'];
+        let pets = JSON.parse(petsJSON);
+        let bodyJSON = JSON.parse(body)
+
+         
+        if(typeof(bodyJSON.age) === "number" && neededKeys.every(key => Object.keys(bodyJSON).includes(key))){
+            let newPet = {}
+            newPet.age = bodyJSON.age;
+            newPet.kind = bodyJSON.kind;
+            newPet.name = bodyJSON.name;
+            pets.push(newPet);
+
+            let petsJSONstr = JSON.stringify(pets);
+            
+            try {
+                fs.writeFileSync(petsPath, petsJSONstr)
+            } catch (err) {
+            console.error(err)
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.write("Pet Added")
+            res.end();
+        } else {
+            res.statusCode = 400;
+            res.setHeader('Content-Type', 'text/plain');
+            res.write('Bad Request')
+            res.end();
+        }
+        
+      }
+      req.on('end', function () {
+        console.log('POSTed: ' + body);
+        res.writeHead(200);
+        res.end();
+    });
+    });
+  }
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Not Found')
+    res.end();
+  }
+});
+
+server.listen(port, function() {
+  console.log('Listening on port', port);
+});
+
+module.exports = server
